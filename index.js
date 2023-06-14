bfc67ef52f48a63a8920b712ffb6d952df55afca
outaagunga <jeconiaagunga@gmail.com>
2023-06-14 13:55:54 +0300

    completed all tests

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+ // Write your code here!
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  }
 }
+ 
 
-function switchOnCharmFromTip(){
   // Write your code here!
+
+  function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
+
+
+ // Write your code here!
+
+
+ function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
